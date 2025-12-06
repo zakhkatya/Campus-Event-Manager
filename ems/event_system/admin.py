4c7c18bbd19b7f82db339c593f9164c963163567
Jakub Vorel <m322903@alunos.ipb.pt>
2025-12-06 13:15:51 +0000

    register UserProfile model

--- ems/event_system/admin.py
+++ ems/event_system/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Event, Registration, Feedback
+from .models import UserProfile, Event, Registration, Feedback
 
 # Register your models here.
-
+admin.site.register(UserProfile)
 admin.site.register(Event)
 admin.site.register(Registration)
 admin.site.register(Feedback)
