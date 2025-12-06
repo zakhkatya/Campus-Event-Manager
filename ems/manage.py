084674d2fb27230c401ae0186703b9816887f6e9
Jakub Vorel <m322903@alunos.ipb.pt>
2025-12-06 13:23:47 +0000

    fixed typo

--- ems/manage.py
+++ ems/manage.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oms.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ems.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
