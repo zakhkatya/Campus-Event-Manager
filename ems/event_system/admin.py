6cfad6392a45a2c1095242842869dae9a2b23a3b
Jakub Vorel <m322903@alunos.ipb.pt>
2025-12-06 13:09:34 +0000

    registered Event, Registration and Feedback models

--- ems/event_system/admin.py
+++ ems/event_system/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, Registration, Feedback
 
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Registration)
+admin.site.register(Feedback)
