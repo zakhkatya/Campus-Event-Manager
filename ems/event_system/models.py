7620138d2108a240587593df501f7ab7929125a5
Jakub Vorel <m322903@alunos.ipb.pt>
2025-12-06 13:08:21 +0000

    add Registration model

--- ems/event_system/models.py
+++ ems/event_system/models.py
@@ -17,3 +17,11 @@ class Event(models.Model):
     def __str__(self):
         return self.title
 
+class Registration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    registered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} → {self.event.title}"
+    
