3c5f4a2bf4a8086b46b2902ab1ded457be5848d5
Kateryna Zakharchuk <m323384@alunos.ipb.pt>
2025-12-08 15:38:56 +0000

    change template name

--- ems/userauth/views.py
+++ ems/userauth/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView
 
 class CustomLoginView(LoginView):
-    template_name = "userauth/login.html"
+    template_name = "userauth/auth_home.html"
     redirect_authenticated_user = True
