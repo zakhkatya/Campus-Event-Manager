2d0ae2bf11bafce2eef077190391a902c844aa6f
Kateryna Zakharchuk <m323384@alunos.ipb.pt>
2025-12-13 19:39:48 +0000

    add views for My Events and Upcoming Events, urls for my-events and upcoming-events

--- ems/event_system/views.py
+++ ems/event_system/views.py
@@ -8,3 +8,17 @@ class HomePageView(View):
 class DashboardView(View):
    def get(self, request):
       return render(request, 'event_system/dashboard.html')
+   
+class MyEventsView(View):
+   def get(self, request, title):
+      return render(request, 'event_system/events.html', {
+         "My events":title,
+         # events 
+      })
+
+class UpcomingEventsView(View):
+   def get(self, request, title):
+      return render(request, 'event_system/events.html', {
+         "Upcoming events":title,
+         #events
+      })
