41b6f033d8b796662e1c27ec6dc1607942760695
Kateryna Zakharchuk <m323384@alunos.ipb.pt>
2025-12-15 17:26:33 +0000

    make templates for login and registration, change views and path for userauth

--- ems/userauth/urls.py
+++ ems/userauth/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, RegisterView
 from .views import CustomLoginView
 
 app_name = 'userauth'
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", RegisterView.as_view(), name="register"),
 ]
--- ems/userauth/views.py
+++ ems/userauth/views.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.views import LoginView
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+# from .forms import CustomRegistrationForm
 
 class CustomLoginView(LoginView):
-    template_name = "userauth/auth_home.html"
+    template_name = "userauth/login.html"
     redirect_authenticated_user = True
+
+class RegisterView(CreateView):
+    template_name = "userauth/register.html"
+    # form_class = CustomRegistrationForm
+    success_url = reverse_lazy("userauth:login")
