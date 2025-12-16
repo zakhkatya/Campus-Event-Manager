ef20ad5a742ac0d28959e46fa72660f4376d46da
Baris Efe Turkmen <m323031@alunos.ipb.pt>
2025-12-16 23:39:17 +0300

    created new model for authing

--- ems/userauth/models.py
+++ ems/userauth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(("email address"), unique=True)
+    username = models.CharField(max_length=150, unique=False, null=True, blank=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [] 
