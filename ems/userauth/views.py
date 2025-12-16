c82120695bad926dd9fced10e52b143a7553c8d0
Baris Efe Turkmen <m323031@alunos.ipb.pt>
2025-12-16 23:39:42 +0300

    create new view and disable auto login

--- ems/userauth/views.py
+++ ems/userauth/views.py
@@ -1,13 +1,24 @@
+# userauth/views.py
+
 from django.contrib.auth.views import LoginView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect 
+from django.contrib.auth import logout 
 from .forms import CustomRegistrationForm
 
 class CustomLoginView(LoginView):
     template_name = "userauth/login.html"
-    redirect_authenticated_user = True
+    redirect_authenticated_user = True 
 
 class RegisterView(CreateView):
     template_name = "userauth/register.html"
     form_class = CustomRegistrationForm
     success_url = reverse_lazy("userauth:login")
+
+    def form_valid(self, form):
+        self.object = form.save()
+        if self.request.user.is_authenticated:
+            logout(self.request) 
+
+        return HttpResponseRedirect(self.get_success_url())
