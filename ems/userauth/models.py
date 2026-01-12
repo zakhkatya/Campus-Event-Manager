5d8129c43e5bc4ad2fea31333aa064ff1fb20341
Baris Efe Turkmen <m323031@alunos.ipb.pt>
2026-01-12 14:29:37 +0300

    changed function for show new avatar of same user and delete old

--- ems/userauth/models.py
+++ ems/userauth/models.py
@@ -4,11 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 def user_avatar_path(instance, filename):
     ext = filename.split('.')[-1]
-    name = instance.first_name.lower().replace(" ", "_") if instance.first_name else "user"
-    role = instance.role.lower() if instance.role else "student"
-    filename = f"{name}_{role}_profile_photo.{ext}"
-
-    return os.path.join('avatars/', filename)
+    return f'avatars/{instance.id}.{ext}'
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
@@ -20,13 +16,21 @@ class CustomUser(AbstractUser):
     email = models.EmailField("email address", unique=True)
     username = models.CharField(max_length=150, unique=False, null=True, blank=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
-
     avatar = models.ImageField(upload_to=user_avatar_path, null=True, blank=True)
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
     def save(self, *args, **kwargs):
+        if self.pk:
+            try:
+                old_avatar = CustomUser.objects.get(pk=self.pk).avatar
+                if old_avatar and self.avatar and old_avatar != self.avatar:
+                    if os.path.isfile(old_avatar.path):
+                        os.remove(old_avatar.path)
+            except CustomUser.DoesNotExist:
+                pass
+
         if self.is_superuser:
             self.role = 'admin'
         super().save(*args, **kwargs)
