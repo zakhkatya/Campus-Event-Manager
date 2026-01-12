89421f682da697326d89b7c29ca089ec74b5f818
Baris Efe Turkmen <m323031@alunos.ipb.pt>
2026-01-12 15:23:44 +0300

    categoried alert messages by changes

--- ems/userauth/views.py
+++ ems/userauth/views.py
@@ -45,6 +45,7 @@ def edit_profile_view(request):
     if request.method == "POST":
         form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
+            changed_fields = form.changed_data
             user = form.save(commit=False)
             new_pwd = form.cleaned_data.get('new_password')
             
@@ -54,14 +55,22 @@ def edit_profile_view(request):
                     user.set_password(new_pwd)
                     user.save()
                     update_session_auth_hash(request, user)
-                    messages.success(request, "Your password have been successfully updated.")
+                    messages.info(request, "Your password has been successfully updated.")
                 except ValidationError as e:
                     for error in e.messages:
                         messages.error(request, error)
                     return render(request, "userauth/edit_profile.html", {"form": form})
+            
+            elif 'avatar' in changed_fields:
+                user.save()
+                messages.info(request, "Your profile photo has been updated.")
+            
+            elif any(field in changed_fields for field in ['first_name', 'last_name', 'email']):
+                user.save()
+                messages.info(request, "Your profile information has been updated.")
+            
             else:
                 user.save()
-                messages.info(request, "Profile updated.")
             
             return redirect("userauth:edit-profile")
     else:
