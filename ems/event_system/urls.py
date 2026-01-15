5b866d1cf3b1e34fba5870608b595206c75f4952
Jakub Vorel <m322903@alunos.ipb.pt>
2026-01-15 16:51:29 +0000

    add route for create and edit event

--- ems/event_system/urls.py
+++ ems/event_system/urls.py
@@ -4,20 +4,40 @@ from . import views
 
 app_name = 'event_system'
 urlpatterns = [
+    # Home page
     path("", HomePageView.as_view(), name="home"),
+    
+    # Dashboard
     path("dashboard", DashboardView.as_view(), name="dashboard"),
+    
+    # My Events
     path("my-events", MyEventsView.as_view(), name="my-events" ),
+    
+    # Upcoming Events
     path("upcoming-events", UpcomingEventsView.as_view(), name="upcoming-events"),
+    
+    # Notifications
     path("notifications", NotificationsView.as_view(), name="notifications"),
+    
+    # Event Details and Actions
     path("register/<int:event_id>/", views.register_for_event, name='register_event'),
     path('unregister/<int:event_id>/', views.unregister_from_event, name='unregister_event'),
     path('delete/<int:event_id>/', views.delete_event, name='delete_event'),
     path('manage/<int:event_id>/<str:status>/', views.manage_status, name='manage_event'),
-    path('add/', views.add_event, name='add_event'),
+    path("create/", views.event_create, name="event_create"),
+    path("<int:pk>/edit/", views.event_edit, name="event_edit"),
     path("event/<int:event_id>/", EventDetailView.as_view(), name="event_detail"),
+    
+    # Approval of Events
     path('approve-events/', views.ApproveEventsListView.as_view(), name='approve-events-list'),
+    
+    # Feedback Views
     path("my-feedbacks", views.MyFeedbacksView.as_view(), name="my-feedbacks"), 
     path("received-feedbacks", views.ReceivedFeedbacksView.as_view(), name="received-feedbacks"),
+    
+    # QR Code for Registrations
     path("registrations/<int:registration_id>/qr/", registration_qr_view, name="registration_qr"),
+    
+    # Past Events
     path("past-events", views.PastEventsView.as_view(), name="past-events"),
 ]
