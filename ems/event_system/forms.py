d235cf941c8327a100c080af7571835ea1f74542
Jakub Vorel <m322903@alunos.ipb.pt>
2026-01-15 16:50:54 +0000

    add EventForm for editing and creating event

---
+++ ems/event_system/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from .models import Event
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = [
+            "title",
+            "location",
+            "date_start",
+            "date_end",
+            "category",
+            "description",
+            "banner",
+        ]
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "class": "form-control form-control-lg",
+                "placeholder": "Event title"
+            }),
+            "location": forms.TextInput(attrs={
+                "class": "form-control",
+                "placeholder": "Event location"
+            }),
+            "date_start": forms.DateTimeInput(
+                attrs={"class": "form-control", "type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M"
+            ),
+            "date_end": forms.DateTimeInput(
+                attrs={"class": "form-control", "type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M"
+            ),
+            "category": forms.Select(attrs={
+                "class": "form-select"
+            }),
+            "description": forms.Textarea(attrs={
+                "class": "form-control",
+                "rows": 4,
+                "placeholder": "Describe the event..."
+            }),
+            "banner": forms.ClearableFileInput(attrs={
+                "class": "form-control"
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["date_start"].input_formats = ["%Y-%m-%dT%H:%M"]
+        self.fields["date_end"].input_formats = ["%Y-%m-%dT%H:%M"]
+        self.fields["category"].empty_label = "Select Category"
